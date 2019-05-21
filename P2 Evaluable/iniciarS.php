7969170ad862570de026e68b28dd38f00bcfd6f7
Saytes <saytes@correo.ugr.es>
2019-05-21 19:46:37 +0200

    Iniciar sesion y cerrar sesion hechos[A

---
+++ P2 Evaluable/iniciarS.php
@@ -0,0 +1,33 @@
+<?php
+
+    $dbhost = 'localhost';
+    $dbuser = 'x75930719';
+    $dbpass = '75930719';
+    $dbname = 'db75930719_pw1819';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+
+    if(!$conn ) {
+        header("Location: ./header.php");
+    }
+    else{
+        session_start();
+    }
+
+    if( (isset($_POST['email'])) and (isset($_POST['pass']))){
+        $email = $_POST['email'];
+        $sql = "SELECT NAME FROM USERS WHERE EMAIL= '$email'";
+        $result = mysqli_query($conn, $sql);
+    }    
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['username']= $row["NAME"];        
+    }
+    else{
+        $_SESSION['username']= 'bbbb';
+    }
+
+    mysqli_close($conn);
+
+    header("Location: ./index.php");
+?>
--- P2 Evaluable/inicioS.php
+++ P2 Evaluable/inicioS.php
@@ -2,10 +2,10 @@
 
     session_start();
 
-    if(isset($_SESSION['logged'])){
+    if(isset($_SESSION['username'])){
 
         echo '<p>';
-            echo  $_SESSION['nombre'];
+            echo  $_SESSION['username'];
         echo  '</p>
             <h3>Est&aacute;s conectado</h3>
             <form name="myform" action="cerrarS.php" method="POST">
@@ -16,8 +16,8 @@
     else{
         echo '<form action="iniciarS.php" id="myform" name="myform" method="post">
                     <section>
-                        <label for="user"><i>Usuario:</i></label>
-                        <input type="text" name="user" id="user" placeholder="Nombre de Usuario" required/>
+                        <label for="user"><i>Email:</i></label>
+                        <input type="text" name="email" id="email" placeholder="Email" required/>
                         </br>
                         <label for="pass"><i>Contrase&ntilde;a:</i></label>
                         <input type="password" name="pass" id="pass" placeholder="Contrase&ntilde;a" required/>
