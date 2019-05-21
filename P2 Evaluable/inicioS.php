d5754f5dbfe55e23f3325e8b02eaf39de96d00c4
Saytes <saytes@correo.ugr.es>
2019-05-21 21:24:51 +0200

    Depuracion del código en inicioS

--- P2 Evaluable/inicioS.php
+++ P2 Evaluable/inicioS.php
@@ -27,31 +27,20 @@
         ';
 
     }
-    else if(isset($_SESSION['error'])){
-        echo '
-                    <script>
-                        function errorLogin() {
-                            alert("Email o Contraseña incorrectos.");
-                        }
-                        errorLogin();
-                    </script>
-                    <form action="iniciarS.php" id="myform" name="myform" method="post">
-                        <section>
-                            <label for="user"><i>Email:</i></label>
-                            <input type="text" name="email" id="email" placeholder="Email" required/>
-                            </br>
-                            <label for="pass"><i>Contrase&ntilde;a:</i></label>
-                            <input type="password" name="pass" id="pass" placeholder="Contrase&ntilde;a" required/>
-                        </section>
-                        <input type="submit" name="submit" value="Enviar">
-                    </form>  
-                    <a href="altausuario.php"><i>Formulario de alta</i></a>
-                </section>
-            </header>   
-        ';  
-        unset($_SESSION['error']);
-    }
     else{
+
+        if(isset($_SESSION['error'])){
+            echo '
+                        <script>
+                            function errorLogin() {
+                                alert("Email o Contraseña incorrectos.");
+                            }
+                            errorLogin();
+                        </script>
+            ';
+            unset($_SESSION['error']);
+        }
+
         echo '
                     <form action="iniciarS.php" id="myform" name="myform" method="post">
                         <section>
